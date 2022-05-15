4ad864c5b109b2b86559f11d3622824908e9a8e8
Doug de Jesus <douglasdejesus92@gmail.com>
2022-05-15 11:20:36 -0400

    Use timer for more accurate sampling rate

---
+++ src/constants.h
@@ -0,0 +1,13 @@
+#ifndef CONSTANTS_H
+#define CONSTANTS_H
+
+#define SAMPLE_PERIOD 200
+
+#define MAX_PRESSURE 150
+#define MIN_PRESSURE 30
+
+#define MAX_DEFLATION_RATE 4
+
+#define PRESSURE_BUFFER_SIZE 1000
+
+#endif
